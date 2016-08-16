92397ae9d4ec565bedea14ce77720051973f9d51
zhaopei <zhaopei0418@aol.com>
2016-08-16 17:56:46 +0800

    添加springmvc测试‘

--- src/main/java/com/hnjiandao/user/controller/UserController.java
+++ src/main/java/com/hnjiandao/user/controller/UserController.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.hnjiandao.user.domain.User;
 import com.hnjiandao.user.service.UserService;
@@ -17,12 +18,25 @@ public class UserController {
 	@Resource
 	private UserService userService;
 	
-	@RequestMapping("/showUser")
+	@RequestMapping("/index")
 	public String toIndex(HttpServletRequest request, Model model) {
 		String userId = request.getParameter("user_id");
 		User user = this.userService.getUserById(userId);
 		System.out.println(user.getUserId() + "[111]" + user.getUserName());
-		
+		model.addAttribute("user", user);
 		return "index";
+	}
+	
+	@RequestMapping
+	public ModelAndView userList(HttpServletRequest request, Model model) {
+		ModelAndView mv = new ModelAndView();
+		
+		String userId = request.getParameter("user_id");
+		User user = this.userService.getUserById(userId);
+		
+		mv.addObject("user", user);
+		mv.setViewName("user");
+		
+		return mv;
 	}
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,12 @@
-<html>
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt"%>
+<%@ page isELIgnored="false" %>
+<!DOCTYPE html>
+<html lang="en">
 <body>
-<h2>Hello World!</h2>
+<h2>Hello World!
+${ user.userName }
+</h2>
 </body>
 </html>
