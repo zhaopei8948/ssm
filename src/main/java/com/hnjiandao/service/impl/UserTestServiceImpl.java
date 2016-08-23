61e627eb4859b94e734546f173b054de1b4d2fda
maozhiping <zhiping.mao@hnjiandao.com>
2016-08-23 17:50:24 +0800

    最后正确版本

--- src/main/java/com/hnjiandao/service/impl/UserTestServiceImpl.java
+++ src/main/java/com/hnjiandao/service/impl/UserTestServiceImpl.java
@@ -15,25 +15,25 @@ public class UserTestServiceImpl implements UserTestService {
 	@Autowired
 	private UserTestMapper userTestMapper;
 	
-	@Override
+
 	public int insert(UserTest userTest) {
 		// TODO Auto-generated method stub
 		return this.userTestMapper.insert(userTest);
 	}
 
-	@Override
+
 	public UserTest selectByPrimaryKey(String id) {
 		// TODO Auto-generated method stub
 		return this.userTestMapper.selectByPrimaryKey(id);
 	}
 
-	@Override
+
 	public long count(UserTest userTest) {
 		// TODO Auto-generated method stub
 		return this.userTestMapper.count(userTest);
 	}
 
-	@Override
+
 	public List<UserTest> selectAll() {
 		// TODO Auto-generated method stub
 		return this.userTestMapper.selectAll();
