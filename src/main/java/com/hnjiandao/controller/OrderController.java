cfa8251e73343d7fd583e54b4febcbdeac32ef36
maozhiping <zhiping.mao@hnjiandao.com>
2016-08-25 15:40:43 +0800

    保存订单

--- src/main/java/com/hnjiandao/controller/OrderController.java
+++ src/main/java/com/hnjiandao/controller/OrderController.java
@@ -1,23 +1,24 @@
 package com.hnjiandao.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-
 import com.hnjiandao.domain.Order;
 import com.hnjiandao.service.OrderService;
-import com.hnjiandao.service.impl.OrderServiceImpl;
 
 @Controller
 @RequestMapping("/order")
 public class OrderController {
+	@Autowired
+	 private OrderService orderService;
+	
 	@RequestMapping(value="/newOrder",method=RequestMethod.POST)  
 	@ResponseBody 
-	public Order newOrder(String orderJson){
-		OrderService service=new OrderServiceImpl();
-		Order order=service.parseOrder(orderJson);
-		System.out.println(order);
+	public Order newOrder(String orderJson){		
+		Order order=orderService.parseOrder(orderJson);
+		orderService.saveOrder(order);
 		return order;
 	}
 }
--- src/main/java/com/hnjiandao/service/impl/OrderServiceImpl.java
+++ src/main/java/com/hnjiandao/service/impl/OrderServiceImpl.java
@@ -1,14 +1,30 @@
 package com.hnjiandao.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.hnjiandao.dao.OverallDataBodyMapper;
+import com.hnjiandao.dao.OverallDataHeadMapper;
+import com.hnjiandao.dao.UserMapper;
 import com.hnjiandao.domain.Order;
 import com.hnjiandao.domain.OrderDetail;
 import com.hnjiandao.domain.OrderHead;
+import com.hnjiandao.domain.OverallDataBody;
+import com.hnjiandao.domain.OverallDataHead;
 import com.hnjiandao.service.OrderService;
+import com.hnjiandao.util.UUIDHelper;
 
 import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
 
+@Service("orderService")
 public class OrderServiceImpl implements OrderService {
+	@Autowired
+	private OverallDataHeadMapper orderheader;
+	
+	@Autowired
+	private OverallDataBodyMapper orderDetail;
+	
 	public Order parseOrder(String orderJson) {
 		Order order = new Order();
 
@@ -26,6 +42,14 @@ public class OrderServiceImpl implements OrderService {
 	}
 
 	public Boolean saveOrder(Order order) {
+		OverallDataHead head = new OverallDataHead(UUIDHelper.getUUID(), order.getOrderHead());
+		orderheader.insertSelective(head);
+
+		for (OrderDetail detail : order.getOrderList()) {
+			OverallDataBody body = new OverallDataBody(UUIDHelper.getUUID(), head.getId(), head.getOrderno(),detail);
+			orderDetail.insertSelective(body);
+		}
+	
 
 		return null;
 	}
