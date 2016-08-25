50c8d929499a68acbad7ca11b16460d1cdff696c
“e <fengju.ding@hnjiandao.com>
2016-08-25 20:50:54 +0800

    fix bug

--- src/main/java/com/hnjiandao/service/impl/OrderServiceImpl.java
+++ src/main/java/com/hnjiandao/service/impl/OrderServiceImpl.java
@@ -12,7 +12,7 @@ import com.hnjiandao.domain.OrderHead;
 import com.hnjiandao.domain.OverallDataBody;
 import com.hnjiandao.domain.OverallDataHead;
 import com.hnjiandao.service.OrderService;
-import com.hnjiandao.util.UUIDHelper;
+import com.hnjiandao.util.Helper;
 
 import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
@@ -42,13 +42,13 @@ public class OrderServiceImpl implements OrderService {
 	}
 
 	public Boolean saveOrder(Order order) {
-		OverallDataHead head = new OverallDataHead(UUIDHelper.getUUID(), order.getOrderHead());
+		OverallDataHead head = new OverallDataHead(Helper.getUUID(), order.getOrderHead());
 
 		if (orderheader.isExists(order.getOrderHead().getOrderNo()) == 0) {
 			orderheader.insertSelective(head);
 
 			for (OrderDetail detail : order.getOrderList()) {
-				OverallDataBody body = new OverallDataBody(UUIDHelper.getUUID(), head.getId(), head.getOrderno(),
+				OverallDataBody body = new OverallDataBody(Helper.getUUID(), head.getId(), head.getOrderno(),
 						detail);
 				orderDetail.insertSelective(body);
 			}
@@ -61,7 +61,7 @@ public class OrderServiceImpl implements OrderService {
 	}
 
 	public Boolean updateOrder(Order order) {
-		OverallDataHead head = new OverallDataHead(UUIDHelper.getUUID(), order.getOrderHead());
+		OverallDataHead head = new OverallDataHead(Helper.getUUID(), order.getOrderHead());
 		orderheader.updateByOrderNoSelective(head);
 
 		orderDetail.deleteByOrderId(head.getOrderno());
@@ -69,7 +69,7 @@ public class OrderServiceImpl implements OrderService {
 		String orderId = orderheader.getOrderId(head.getOrderno());
 
 		for (OrderDetail detail : order.getOrderList()) {
-			OverallDataBody body = new OverallDataBody(UUIDHelper.getUUID(), orderId, head.getOrderno(), detail);
+			OverallDataBody body = new OverallDataBody(Helper.getUUID(), orderId, head.getOrderno(), detail);
 			orderDetail.insertSelective(body);
 		}
 
