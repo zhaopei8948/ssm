d5f2956b2bf290ca1e337f993f10cc0dd63326f5
dingfengju <fengju.ding@hnjiandao.com>
2016-09-07 11:07:32 +0800

    常量

---
+++ src/main/java/com/hnjiandao/util/Const.java
@@ -0,0 +1,8 @@
+package com.hnjiandao.util;
+
+public class Const {
+		
+	public static final String LOGIN_URL = "jsp/session/login"; //登陆链接4
+	public static final String NO_PREVENT_URL = ".*/((login)|(logout)|(signup)|(createLogin)|(addUser)|(.ss)|(.js)|(.woff)|(public)|(.jpg)|(.png)).*"; //开放链接正则
+	
+}
